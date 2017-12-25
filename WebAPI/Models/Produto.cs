ac27a25ed07e1fc08c3cb55acb7976697999842e
Johnatan Tavares <johnatan12014@hotmail.com>
2017-12-25 12:26:41 -0200

    Classe Produto e ProdutoController

---
+++ WebAPI/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Produto
+    {
+        //Atributos
+        private int codigo;
+        private string nome;
+        private string categoria;
+        private decimal valor;
+
+        //Gets e Sets
+        public int Codigo { get { return codigo; } set { codigo = value; } }
+        public string Nome { get { return nome; } set { nome = value; } }
+        public string Categoria { get { return categoria; } set { categoria = value; } }
+        public decimal Valor { get { return valor; } set { valor = value; } }
+
+    }
+}
